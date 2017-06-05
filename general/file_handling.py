5d05f7ea34e381c1ef5fc5bbf40f7ea5e5774c35
Manuel <info@tagirijus.de>
2017-06-05 13:00:14 +0200

    Introduced new file handling system

--- general/file_handling.py
+++ general/file_handling.py
@@ -1,3 +1,80 @@
-"""Module for savign and loading MIDICue objects to / from files."""
+"""Module for saving and loading a string to / from a file."""
 
 import os
+
+
+class FileObject(object):
+    """The FileObject for saving and loading a string."""
+
+    def __init__(self, file=None):
+        """Initialize the class."""
+        self.file = file
+
+    def validate_file(self):
+        """Check if file is valid."""
+        # check if file exists already
+        if os.path.exists(self._file):
+            self._new_file = False
+        else:
+            self._new_file = True
+
+        # if it's a dir it's not valid
+        if os.path.isdir(self._file):
+            self._valid = False
+        else:
+            self._valid = True
+
+    @property
+    def file(self):
+        """Get file."""
+        return self._file
+
+    @file.setter
+    def file(self, value=None):
+        """Set the file and validate it."""
+        self._file = str(value)
+        self.validate_file()
+
+    @property
+    def valid(self):
+        """Get valid."""
+        return self._valid
+
+    @valid.setter
+    def valid(self, value=None):
+        """Basically do nothing - valid is not setable, but must be checked."""
+        self.validate_file()
+
+    @property
+    def new_file(self):
+        """Get new_file."""
+        return self._new_file
+
+    @new_file.setter
+    def new_file(self, value=None):
+        """Basically do nothing - new_file is not setable, but must be checked."""
+        self.validate_file()
+
+    def save(self, string=None):
+        """Save the string to the file."""
+        # cancel if filename is not valid
+        if not self.valid:
+            return 'No valid file.'
+
+        # save the string
+        with open(self.file, 'w') as f:
+            f.write(str(string))
+
+        return True
+
+    def load(self, string=None):
+        """Load the string from file.."""
+        # cancel if filename is not valid
+        if not self.valid:
+            return ''
+
+        # load the string
+        with open(self.file, 'w') as f:
+            out = f.read()
+
+        return out
--- npy_gui/npy_gui.py
+++ npy_gui/npy_gui.py
@@ -1,5 +1,6 @@
 """The graphical user interface for the ledgeradd programm."""
 
+from general import file_handling
 from general import midicuer
 import npyscreen
 
@@ -7,12 +8,14 @@ import npyscreen
 class midicuerApplication(npyscreen.NPSAppManaged):
     """The main application object."""
 
-    def __init__(self, settings, presets, *args, **kwargs):
+    def __init__(self, file=None, *args, **kwargs):
         """Initialize the class."""
         super(midicuerApplication, self).__init__(*args, **kwargs)
 
         # set global temp variables
         self.tmpMIDICues = []
+
+        # set file
 
 
     def onStart(self):
